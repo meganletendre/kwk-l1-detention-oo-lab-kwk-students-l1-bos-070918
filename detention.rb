6b0873b8dd7e6d3148c4e3a39fbbae0bb4f53446
meganletendre <megan.letendre@comcast.net>
2018-07-13 15:55:07 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,7 +1,8 @@
 #Code your detention class here
 class Detention 
-  attr_accessor :location
-  def intialize(location = "Room 301")
+  attr_accessor :location, :time 
+  def intialize(location = "Room 301", time = "3:00 PM")
     @location = location
+    @time = time 
   end 
 end 
