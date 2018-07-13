fb8d622ba8b43d669cda14c5497bbeb66d6dea18
meganletendre <megan.letendre@comcast.net>
2018-07-13 15:59:10 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,7 +1,7 @@
 #Code your detention class here
 class Detention 
   attr_accessor :location, :time, :grumpy_teacher, :activity 
-  def intialize(location = "Room 301", time = "3:00 PM", grumpy_teacher = "John Schmidt", activity = "collective punishment")
+  def initialize(location = "Room 301", time = "3:00 PM", grumpy_teacher = "John Schmidt", activity = "collective punishment")
     @location = location
     @time = time 
     @grumpy_teacher = grumpy_teacher
